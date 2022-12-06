50636ddd4977068957ac556f7b8c562fce699104
Adam Kaczmarski <adam.kacz10@gmail.com>
2022-12-06 11:37:29 +0000

    day_6

---
+++ day_6/a.py
@@ -0,0 +1,28 @@
+def get_marker(line):
+    line = line.strip()
+    tmp=[]
+    for idx,char in enumerate(list(line)):
+        if(len(tmp)==4):
+            return idx
+        if char not in tmp:
+            tmp.append(char)
+        else:
+            right = tmp[tmp.index(char)+1:]
+            tmp=right
+            tmp.append(char)
+
+
+
+def main():
+    with  open('./input.prod') as file:
+        lines = file.readlines();
+        for line in lines:
+            print(line)
+            print(get_marker(line))
+                    
+            
+
+
+
+if __name__ == '__main__':
+    main()
