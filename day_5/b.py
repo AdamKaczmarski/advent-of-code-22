f7c254cfaa5d8138640bb8dc27b3139e02cfeca5
Adam Kaczmarski <adam.kacz10@gmail.com>
2022-12-06 10:38:58 +0000

    day5

---
+++ day_5/b.py
@@ -0,0 +1,35 @@
+import re
+crateStacks = {}
+def movecrate(amount,fromStack,toStack):
+    if len(crateStacks[fromStack])!=0:
+        crates = crateStacks[fromStack][0:int(amount)]
+        crateStacks[fromStack] = crateStacks.get(fromStack)[int(amount):]
+        crateStacks[toStack]=crates+crateStacks[toStack]
+
+def main():
+    crates = [] 
+    with  open('./input.prod') as file:
+        data = file.read().split("\n\n")
+    print(data[0])
+    crates = (data[0].split("\n"))
+    moves = data[1].split('\n')
+    moves = moves[:len(moves)-1]
+    indexline = crates[len(crates)-1:][0]
+    for idx in indexline.split():
+        crateStacks[idx]=[]
+    for line in crates[:len(crates)-1]:
+        letters = re.findall(r'\w',line)
+        for letter in letters:
+            idx = line.index(letter)
+            crateStacks[indexline[idx]].append(letter)
+            line=line[:idx]+"_"+line[idx+1:]
+    for move in moves:
+        whatwhere = re.findall(r"\d+",move)
+        movecrate(whatwhere[0],whatwhere[1],whatwhere[2])
+    result=""
+    for idx,stack in crateStacks.items():
+        result+=stack[0]
+    print(result)
+
+if __name__ == '__main__':
+    main()
