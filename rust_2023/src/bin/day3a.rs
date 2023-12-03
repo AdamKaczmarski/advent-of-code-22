bb9b3cc875049ff1baa2a6776c1b6d28aa2b1e9b
AdamKaczmarski <adam.kacz10@gmail.com>
2023-12-03 22:33:52 +0000

    [day3] don't ask me about performance

--- rust_2023/src/bin/day3a.rs
+++ rust_2023/src/bin/day3a.rs
@@ -1,145 +1,122 @@
 use anyhow::Result;
 use itertools::Itertools;
-use regex::Regex;
-use std::{char, fs};
+use std::fs;
 
-fn extract_numbers_from_line(line: &str) -> Vec<&str> {
-    // I don't have the license to use regex but I struggled before that, ok
-    // regexlicensing.org
-    let re = Regex::new(r"([0-9]+)").unwrap();
-    let res = re
-        .find_iter(line)
-        .map(|number_str| number_str.as_str())
-        .collect_vec();
-    println!("{:?}",res);
-    return res
-}
-
-fn is_number_valid(start_idx: usize, l_to_take: usize, line: &str, check_line: &str) -> bool {
-    let mut start = start_idx;
-    let mut end = start_idx + l_to_take;
-    if start_idx != 0 {
-        //check left side INLINE
-        start = start_idx - 1;
-        let character: char = line.chars().nth(start).unwrap();
-        if !character.is_digit(10) && character.ne(&'.') {
-            return true;
-        }
-    }
-    if end != line.len() - 1 {
-        // check right side INLINE
-        let character = line.chars().nth(end);
-        match character {
-            Some(x) => {
-                if !x.is_digit(10) && x.ne(&'.') {
-                    return true;
-                }
-            }
-            None => (),
-        }
-        end = l_to_take + 1;
-    }
-    let count = check_line
-        .chars()
-        .skip(start)
-        .take(end + 1)
-        .filter(|c: &char| {
-            // println!("char: {c}");
-            return !c.is_digit(10) && c.ne(&'.');
-        })
-        .count();
-    // println!("count: {count}");
-
-    if count > 0 {
-        return true;
-    }
-
-    return false;
-}
-
-fn get_valid_parsed_number(
-    start_idx: usize,
-    l_to_take: usize,
-    line: &str,
-    line_before: &str,
-) -> usize {
-    if is_number_valid(start_idx, l_to_take, line, line_before) {
-        // println!(
-        //     "valid: {}",
-        //     line[start_idx..start_idx + l_to_take].to_owned()
-        // );
-        return line[start_idx..start_idx + l_to_take]
-            .parse::<usize>()
-            .unwrap();
-    }
-
-    return 0;
-}
-fn get_valid_sum_before_after(
+#[derive(Debug, Clone, Default)]
+struct Number {
     start_idx: usize,
     end_idx: usize,
-    line: &str,
-    line_before: &str,
-    line_after: &str,
-) -> usize {
-    let before_check = get_valid_parsed_number(start_idx, end_idx, line, line_before);
-    let after_check = get_valid_parsed_number(start_idx, end_idx, line, line_after);
-    // println!("line: {} before val:{}, after val:{}", line, before_check, after_check);
-    // println!("valid: {}", std::cmp::max(before_check, after_check));
-    return std::cmp::max(before_check, after_check);
+    value_str: String,
+    value: usize,
+}
+
+impl Number {
+    fn parse_value(&mut self) {
+        self.value = self.value_str.parse::<usize>().unwrap();
+    }
+}
+
+#[derive(Debug, Default)]
+struct Symbol {
+    idx: usize,
+}
+
+#[derive(Debug, Default)]
+struct Line {
+    line_no: usize,
+    symbols: Vec<Symbol>,
+    numbers: Vec<Number>,
+}
+impl Line {
+    fn set_line(&mut self, n: usize) {
+        self.line_no = n;
+    }
 }
 
 fn main() -> Result<()> {
-    panic!("doesn't work");
-    let mut sum: usize = 0;
+    let mut lines: Vec<Line> = vec![];
+    //test2 result = 413
+        fs::read_to_string("./src/bin/day3.prod")?
+            .lines()
+            .enumerate()
+            .for_each(|(idx, line)| {
+                let mut whole_line: Line = Line::default();
+                whole_line.set_line(idx);
+                let chars = line.chars().collect_vec();
 
-    let text = fs::read_to_string("./src/bin/day3.prod")?;
-    let lines_tmp = &text.lines().collect_vec();
-    text.lines().enumerate().for_each(|(idx, line)| {
-        let values: Vec<&str> = extract_numbers_from_line(line);
+                let mut number: Number = Number::default();
+                for (c_idx, char) in chars.iter().enumerate() {
+                    if char.is_digit(10) {
+                        number.end_idx = c_idx;
+                        number.value_str.push(char.to_owned());
+                    } else if !char.is_digit(10) && char.ne(&'.') {
+                        whole_line.symbols.push(Symbol { idx: c_idx });
+                    }
+                    if c_idx > 0 {
+                        match line.chars().nth(c_idx - 1) {
+                            Some(val) => {
+                                if val.is_digit(10)
+                                    && ((char.eq(&'.') || char.is_ascii_punctuation())
+                                        || (val.is_digit(10) && c_idx == chars.len() - 1))
+                                {
+                                    number.start_idx =
+                                        number.end_idx - (number.value_str.len() - 1);
+                                    number.parse_value();
+                                    whole_line.numbers.push(number.clone());
+                                    number = Number::default();
+                                }
+                            }
+                            None => (),
+                        }
+                    }
+                }
+                lines.push(whole_line);
+            });
 
-        for value in values {
-            //TODO thsi search here is wrong, value can be repeated
-            let start_idx = line.find(value).unwrap();
-            let length_to_take = value.len();
-
-            let mut line_before;
-            let mut line_after;
-
-            if idx == 0 {
-                line_before = idx;
-                line_after = idx + 1;
-            } else {
-                line_before = idx - 1;
-                line_after = idx + 1;
-            }
-            if idx == lines_tmp.len() - 1 {
-                line_after = idx;
-                line_before = idx - 1;
-            }
-
-            if line_after != idx && line_before != idx {
-                sum += get_valid_sum_before_after(
-                    start_idx,
-                    length_to_take,
-                    line,
-                    lines_tmp[line_before],
-                    lines_tmp[line_after],
-                );
-            } else if line_after == idx {
-                sum += get_valid_parsed_number(
-                    start_idx,
-                    length_to_take,
-                    line,
-                    lines_tmp[line_before],
-                );
-            } else if line_before == idx {
-                sum +=
-                    get_valid_parsed_number(start_idx, length_to_take, line, lines_tmp[line_after]);
+        let mut sum: usize = 0;
+        for line in &lines {
+            for symbol in &line.symbols {
+                if line.line_no == 0 {
+                    let line_after: &Line = lines.get(line.line_no + 1).unwrap();
+                    sum += get_sum(symbol, line_after);
+                } else if line.line_no == lines.len() - 1 {
+                    let line_before: &Line = lines.get(line.line_no - 1).unwrap();
+                    sum += get_sum(symbol, line_before);
+                } else {
+                    let line_after: &Line = lines.get(line.line_no + 1).unwrap();
+                    sum += get_sum(symbol, line_after);
+                    let line_before: &Line = lines.get(line.line_no - 1).unwrap();
+                    sum += get_sum(symbol, line_before);
+                }
+                for number in &line.numbers {
+                    if number.start_idx != 0 {
+                        if number.start_idx - 1 == symbol.idx {
+                            sum += number.value;
+                        }
+                    }
+                    if number.end_idx + 1 == symbol.idx {
+                        sum += number.value;
+                    }
+                }
             }
         }
-    });
-    println!("{}", sum);
+        println!("sum: {}", sum);
 
     Ok(())
+}
+
+fn get_sum(symbol: &Symbol, check_line: &Line) -> usize {
+    let mut sum = 0;
+    for number in &check_line.numbers {
+        if number.start_idx == 0 {
+            if number.start_idx <= symbol.idx && symbol.idx <= number.end_idx + 1 {
+                sum += number.value;
+            }
+        } else {
+            if number.start_idx - 1 <= symbol.idx && symbol.idx <= number.end_idx + 1 {
+                sum += number.value;
+            }
+        }
+    }
+    return sum;
 }
