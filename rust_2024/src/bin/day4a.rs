13aee529a8b974e9d3bcd95f98d92ea87494ee20
AdamKaczmarski <adam.kacz10@gmail.com>
2024-12-04 21:41:48 +0000

    4a cleaner

--- rust_2024/src/bin/day4a.rs
+++ rust_2024/src/bin/day4a.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::{HashSet, VecDeque},
-    fs,
-};
+use std::{collections::VecDeque, fs};
 
 use anyhow::Result;
 use itertools::Itertools;
@@ -17,7 +14,6 @@ fn main() -> Result<()> {
             .lines()
             .map(|l| String::from(l))
             .collect_vec();
-        let mut visited_x_cords: HashSet<(i32, i32)> = HashSet::new();
         let row: i32 = grid.len().try_into().unwrap();
         let col: i32 = grid.get(0).unwrap().len().try_into().unwrap();
         let mut count = 0;
@@ -25,12 +21,13 @@ fn main() -> Result<()> {
             let row_input = grid.get(r as usize).unwrap();
             for c in 0..col {
                 let char = row_input.chars().nth(c as usize).unwrap();
-                if char != 'X' || visited_x_cords.contains(&(r, c)) {
+                if char != 'X' {
                     continue;
                 }
                 let mut stack: VecDeque<char> = VecDeque::new();
-                count += collect_dfs(&grid, &mut stack, r, c, row, col);
-                visited_x_cords.insert((r, c));
+                for (x, y) in DIRS {
+                    count += dfs(&grid, &mut stack, r, c, row, col, (*x, *y));
+                }
             }
         }
         println!("{file}: {count}");
@@ -49,20 +46,6 @@ const DIRS: &'static [(i32, i32)] = &[
 ];
 
 const MAS: &'static [char] = &['M', 'A', 'S'];
-fn collect_dfs(
-    grid: &Vec<String>,
-    stack: &mut VecDeque<char>,
-    r: i32,
-    c: i32,
-    max_r: i32,
-    max_c: i32,
-) -> i32 {
-    let mut met = 0;
-    for (x, y) in DIRS {
-        met += dfs(grid, stack, r, c, max_r, max_c, (*x, *y));
-    }
-    met
-}
 
 fn dfs(
     grid: &[String],
@@ -73,17 +56,17 @@ fn dfs(
     max_c: i32,
     (x, y): (i32, i32),
 ) -> i32 {
-    let mut met = 0;
     if stack.len() == 3 {
         return 1;
     }
-    let which_xmas_char = stack.len();
-    let xmas_char = MAS[which_xmas_char];
     let r = r + x;
     let c = c + y;
     if r < 0 || c < 0 || r >= max_r || c >= max_c {
         return 0;
     }
+    let mut met = 0;
+    let which_xmas_char = stack.len();
+    let xmas_char = MAS[which_xmas_char];
     let char = grid
         .get(r as usize)
         .unwrap()
