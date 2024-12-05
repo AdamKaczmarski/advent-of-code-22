09c5790295c9805dd9d202b247575dafab8d765c
AdamKaczmarski <adam.kacz10@gmail.com>
2024-12-05 20:55:55 +0000

    day5

---
+++ rust_2024/src/bin/day5a.rs
@@ -0,0 +1,55 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
+
+use anyhow::Result;
+use itertools::Itertools;
+
+fn main() -> Result<()> {
+    let files = ["./inputs/day5.test", "./inputs/day5.prod"];
+    for file in files {
+        let input = fs::read_to_string(file)?;
+        let parts = input.split("\n\n").collect_vec();
+        let mut number_afters: HashMap<i32, HashSet<i32>> = HashMap::new();
+        parts.get(0).unwrap().lines().for_each(|line| {
+            let (x, y) = line
+                .split('|')
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect_tuple()
+                .unwrap();
+            number_afters.entry(x).or_insert(HashSet::new()).insert(y);
+        });
+        let sum: i32 = parts
+            .get(1)
+            .unwrap()
+            .lines()
+            .map(|line| {
+                line.split(",")
+                    .map(|x| x.parse::<i32>().unwrap())
+                    .collect_vec()
+            })
+            .filter(|ordering| {
+                //brute force solution
+                for i in 0..ordering.len() - 1 {
+                    let x = ordering.get(i).unwrap();
+                    match number_afters.get(x) {
+                        Some(set) => {
+                            for j in i + 1..ordering.len() {
+                                let y = ordering.get(j).unwrap();
+                                if !set.contains(y) {
+                                    return false;
+                                }
+                            }
+                        }
+                        None => return false,
+                    }
+                }
+                true
+            })
+            .map(|order| *order.get(order.len() / 2).unwrap())
+            .sum();
+        println!("{file}: {sum}");
+    }
+    Ok(())
+}
